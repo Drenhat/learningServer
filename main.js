2d8b96188e92a7662fc68c12d5588dd0dbcaae68
Tim Huerne <76855940+Drenhat@users.noreply.github.com>
2021-01-30 12:21:14 -0500

    I add a comment in the main.js file to explain what is happening

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import {compilFunc, alrt1, alrt2} from './module.js';
 
+// Trigger the compilFunc. It adds 2 functions as items within the object obj.
 const obj = compilFunc(alrt1('Matt', 8), alrt2(25));
 
 obj.f1();
