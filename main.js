e9ca4f352414208d0c8f5768639c163703862c43
Tim Huerne <76855940+Drenhat@users.noreply.github.com>
2021-01-30 12:17:25 -0500

    first commitment with this code.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+import {compilFunc, alrt1, alrt2} from './module.js';
 
+const obj = compilFunc(alrt1('Matt', 8), alrt2(25));
+
+obj.f1();
+obj.f2();
--- module.js
+++ module.js
@@ -1,1 +1,12 @@
 
+//a function to combine two functions into an object
+const compilFunc = (f1, f2) => {return {f1: f1, f2: f2}}
+
+/* a function with 2 arguments. The first argument should be a string and 
+the second one a string or an integer.*/
+const alrt1 = (name, time) => alert(`Wake up ${name}! It is already ${time} am.`)
+
+// A function that wishes you happy birthday. It takes one argument.
+const alrt2 = (age) => alert(`Happy birthday! Wow, you are already ${age} years old`)
+
+export {compilFunc, alrt1, alrt2};
