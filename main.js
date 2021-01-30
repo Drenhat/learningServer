cdcd56bc384f06747d6e579d3dd781ae388185e8
Tim Huerne <76855940+Drenhat@users.noreply.github.com>
2021-01-30 15:01:30 -0500

    I added some text into the html div

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {compilFunc, alrt1, alrt2} from './module.js';
-document.getElementById('application').innerHTML = hello;
+document.getElementById("application").innerHTML = hello;
 
 // Trigger the compilFunc. It adds 2 functions as items within the object obj.
 const obj = compilFunc(alrt1('Matt', 8), alrt2(25));
