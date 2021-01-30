79846c846d8a602e9bbc3c5288962479c625785c
Tim Huerne <76855940+Drenhat@users.noreply.github.com>
2021-01-30 14:55:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import {compilFunc, alrt1, alrt2} from './module.js';
+document.getElementById('application').innerHTML = hello;
 
 // Trigger the compilFunc. It adds 2 functions as items within the object obj.
 const obj = compilFunc(alrt1('Matt', 8), alrt2(25));
 
+// trigger the functions that are inside obj.
 obj.f1();
 obj.f2();
